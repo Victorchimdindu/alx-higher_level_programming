2d08cd639b9175e5f0f48985b4dad8563585242a
victorchimdindu <you@example.com>
2022-11-29 15:48:17 -0800

    fixed

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,9 +1,6 @@
-Executable File  7 lines (7 sloc)  142 Bytes
-
 #!/usr/bin/python3
+
 def islower(c):
-    check = ord(c)
-    if check >= 97 and check <= 122:
-        return True
-    else:
-        return False
+        if ord(c) >= 97 and ord(c) <= 122:
+                return True
+        return False 
