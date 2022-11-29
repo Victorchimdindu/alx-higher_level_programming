06105d75ca0a0e8ab079bfc5116d45c5b8076417
victorchimdindu <you@example.com>
2022-11-29 15:45:01 -0800

    fixed

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i >= j:
-            continue
-        elif i == 8 and j == 9:
-            print("{}{}".format(i, j))
-        else:
-            print("{}{}, ".format(i, j), end='')
+Executable File  8 lines (7 sloc)  231 Bytes
 
+#!/usr/bin/python3
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print(f"{digit1}{digit2}")
+        else:
+            print(f"{digit1}{digit2}", end=", ")
