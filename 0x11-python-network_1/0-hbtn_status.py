08af9cd070a42a43ebadfb5d122f9984e1dbb654
victorchimdindu <you@example.com>
2023-04-04 18:38:54 -0700

    food

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         print('Body response:')
         print("\t- type: {}".format(type(the_page)))
         print("\t- content: {}".format(the_page))
-        print("\t- utf8 content: {}".format(the_page.decode('utf-8')) 
+        print("\t- utf8 content: {}".format(the_page.decode('utf-8')))
